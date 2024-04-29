f8e07634b0309f36d79b696c1a79412aedfeb552
gorilonse <suarez_dos@hotmail.com>
2024-04-29 00:17:18 -0300

    Ejercicio 2

---
+++ tp2_SuarezDarioOscar/src/ar/edu/unju/fi/ejercicio2/main/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio2.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("holaaaaaa");
+		System.out.println("dddddd");
+	}
+
+}
