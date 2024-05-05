97be45ed58affa090aff2ccd08b77f597c0900b8
gorilonse <suarez_dos@hotmail.com>
2024-05-05 02:19:35 -0300

    test rama 4

---
+++ tp2_SuarezDarioOscar/src/ar/edu/unju/fi/ejercicio4/main/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio4.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("holaaaaa");
+	}
+
+}
